f3337778fb8ebaff39a805dc2b36eae2661339c0
AudroneLT <audrone.vazneliene@gmail.com>
2022-02-24 21:05:29 +0200

    Addet Variables and data

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string name = "Vincentas";
+int age = 10;
+string power1 = "Omnipresent";
+string power2 = "Superstrong";
+string power3 = "Superstrong";
+
+
+Console.WriteLine("*******************************************");
+Console.WriteLine("SuperHero");
+Console.WriteLine("*******************************************");
+
+Console.WriteLine($" Hero name: {name}");
+Console.WriteLine($" Hero age: {age}");
+Console.WriteLine($" Hero superpowers: {power1}, {power2}, {power3}");
